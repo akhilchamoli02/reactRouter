f853aef03e90f6a750cc143db512bfda14adf618
Akhil Chamoli <akhilchamoli02@gmail.com>
2024-08-10 21:26:24 +0530

    Updated Main Components and SetUp Router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom"; //we use link tag instead of a tag to avoid reloading(refreshing) of the page
+import { Link, NavLink } from "react-router-dom"; //we use <link> tag instead of <a> tag to avoid reloading(refreshing) of the page
 
 function Header() {
     return (
@@ -15,7 +15,7 @@ function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="#" //to is equivalent to href
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
@@ -34,14 +34,28 @@ function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                to='/'
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${
+                                            isActive ? "text-orange-700" :"text-gray-700"
+                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
                                 </NavLink>
                             </li>
-                            
+                            <li>
+                                <NavLink
+                                to='/about'
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${
+                                            isActive ? "text-orange-700" :"text-gray-700"
+                                        } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About
+                                </NavLink>
+                            </li>
                             
                         </ul>
                     </div>
